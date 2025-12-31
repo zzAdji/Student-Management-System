7ac952f7189dfd6ebbf40af4acd8cbbed90bf23c
Ntchamba alan <ntchambaalanryan@gmail.com>
2025-12-31 10:55:59 +0100

    finalistion des prototypes recherche

--- include/search.h
+++ include/search.h
@@ -5,14 +5,43 @@
 int binarySearch(Gestion_des_Etudiants* tableau , char matricule,
 int nombre);// prototype de la fonction de tri;
 
+
+
+
+
+
+
+
 int linearSearch
 (Gestion_des_Etudiants* tableau, char* matricule, int nombre);
+
+
+
+
+
+
+
 
 
 int isSorted(Gestion_des_Etudiants* tableau, int nombre);
 
 
+
+
+
+
+
+
+
+
 int smartSearch(Gestion_des_Etudiants* tableau,char* matricule, int nombre);
+
+
+
+
+
+
+
 
 
 #endif
