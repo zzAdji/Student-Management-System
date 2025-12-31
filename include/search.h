5e866fd94e36cdfd2d0f1035c8e6c119f1615c6e
Ntchamba alan <ntchambaalanryan@gmail.com>
2025-12-31 08:51:03 +0100

    protypes fonctions tri et recherche

---
+++ include/search.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH
+#define SEARCH
+
+
+int search(Gestion_des_Etudiants* tableau , int matricule,
+int nombre);// prototype de la fonction de tri;
+
+
+
+
+#endif
---
+++ include/sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT
+#define SORT
+
+
+ void sort(Gestion_des_Etudiants* 
+tableau ,int nombre );
+
+#endif
