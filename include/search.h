c8240e8911297b4ba56637599e81ba217f9e7467
Ntchamba <ntchambaalanryan@gmail.com>
2026-01-04 00:13:47 +0100

    Add files via upload

---
+++ include/search.h
@@ -0,0 +1,47 @@
+#ifndef SEARCH
+#define SEARCH
+
+
+int binarySearch(Gestion_des_Etudiants* tableau , char matricule,
+int nombre);// prototype de la fonction de tri;
+
+
+
+
+
+
+
+
+int linearSearch
+(Gestion_des_Etudiants* tableau, char* matricule, int nombre);
+
+
+
+
+
+
+
+
+
+int isSorted(Gestion_des_Etudiants* tableau, int nombre);
+
+
+
+
+
+
+
+
+
+
+int smartSearch(Gestion_des_Etudiants* tableau,char* matricule, int nombre);
+
+
+
+
+
+
+
+
+
+#endif
---
+++ include/sort.h
@@ -0,0 +1,23 @@
+#ifndef SORT
+#define SORT
+
+
+ 
+
+/// @brief 
+/// @param  management Pointeur vers la structure de gestion
+/// @param nombre number of student
+void sortAlphabetically(Student_Management *management , int 
+nombre);
+
+void sortByOption(Student_Management *management , int 
+nombre);
+
+/// @brief  
+/// @param  management Pointeur vers la structure de gestion
+/// @param nombre number of student
+void sortById(Student_Management *management , int 
+nombre);thr
+
+
+#endif
