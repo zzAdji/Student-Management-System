d56634f5aec09415968d24d6b9def25b4af78085
Ntchamba <ntchambaalanryan@gmail.com>
2026-01-06 01:57:51 +0100

    Ajustment

--- src/sort.c
+++ src/sort.c
@@ -2,43 +2,13 @@
 #include<stdlib.h>
 #include<string.h>
 #include"sort.h"
-// creation de la fonction de tri 
-void  sort(Student* 
-tableau ,int nombre ){
-               
-  
-     for (int i = 1; i < nomre; i++)
-     {
-       
-        Gestion_des_Etudiants stud  = tableau[i];
-         
-         
-        int j = i-1;// intialistion du compteur des cases suivantes;
-
-        while((j>=0)&&(strcmp(stud.nom,tableau[j].nom)< 0)){
+// creation des fonctions de tri 
 
 
-         tableau[j+1] = tableau[j];//Remplacement de la case suivante par la precedente;
+    
 
-         j--;
-
-        }
+    //Tri Par ordre alphabetique;
        
-          tableau[j+1] = stud;// positionnement de la case dans le rang;
-       
-       
-       
-       
-       
-       
-        /* code */
-     }
-     
-
-    }
-
-    // Par ordre alphabetique;
-
 void sortAlphabetically(Student_Management *management , int nombre){
 
 
