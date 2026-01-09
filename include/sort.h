9283dd0298f97395edd7888a2ebef16e55255ff6
Ntchamba alan <ntchambaalanryan@gmail.com>
2026-01-09 03:39:05 +0100

    changement

---
+++ include/sort.h
@@ -0,0 +1,21 @@
+#ifndef SORT
+#define SORT
+#include"student.h"
+
+
+ 
+
+/// @brief 
+/// @param  management Pointeur vers la structure de gestion
+/// @param nombre number of student
+void sortAlphabetically(Student_Management *management );
+
+void sortByOption(Student_Management *management);
+
+/// @brief  
+/// @param  management Pointeur vers la structure de gestion
+/// @param nombre number of student
+void sortById(Student_Management *management);
+
+
+#endif
---
+++ src/sort.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"../include/sort.h"
+// creation des fonctions de tri 
+
+
+    
+
+    //Tri Par ordre alphabetique;
+       
+void sortAlphabetically(Student_Management *management ){
+
+      int nombre = 0;
+      nombre = management->number;
+
+   for (int i = 1; i < nombre; i++)
+   {
+     
+      Student stud  = management->list[i];
+       
+       
+      int j = i-1;// intialistion du compteur des cases suivantes;
+
+      while((j>=0)&&(strcmp(stud.id,management->list[j].name) < 0)){
+       management->list[j+1] = management->list[j];//Remplacement de la casesuivante par la precedente;
+
+       j--;
+      }
+     
+        management->list[j+1] = stud;// positionnement de la case dans le rang;
+     
+
+
+     
+     
+     
+     
+     
+      /* code */
+   }
+
+
+}
+
+
+
+/**
+ * Tri par filière
+ * 
+ * @param management Pointeur vers la structure de gestion
+ */
+void sortByOption(Student_Management *management )
+{
+   int nombre = 0;
+
+   nombre = management->number;
+  
+   for (int i = 1; i < nombre; i++)
+   {
+     
+      Student stud  = management->list[i];
+       
+       
+      int j = i-1;// intialistion du compteur des cases suivantes;
+
+      while((j>=0)&&(strcmp(stud.option,management->list[j].
+option)< 0)){
+       management->list[j+1] = management->list[j];//Remplacement de la casesuivante par la precedente;
+
+       j--;
+      }
+     
+        management->list[j+1] = stud;// positionnement de la case dans le rang;
+     
+
+
+     
+     
+     
+     
+     
+      /* code */
+   }
+};
+
+/**
+ * Tri par matricule (pour recherche dichotomique)
+ * @param management Pointeur vers la structure de gestion
+ */
+void sortById(Student_Management *management){
+  
+int nombre = 0;
+
+nombre = management->number;
+
+
+
+
+   for (int i = 1; i < nombre; i++)
+   {
+     
+      Student stud  = management->list[i];
+       
+       
+      int j = i-1;// intialistion du compteur des cases suivantes;
+
+      while((j>=0)&&(strcmp(stud.id,management->list[j].id)< 0)){
+       management->list[j+1] = management->list[j];//Remplacement de la casesuivante par la precedente;
+
+       j--;
+      }
+     
+        management->list[j+1] = stud;// positionnement de la case dans le rang;
+     
+
+
+     
+     
+     
+     
+     
+      /* code */
+   }
+   
+    
+
+};
