7c5b8e4f24d28b82153e182218543123704e6743
Ntchamba alan <ntchambaalanryan@gmail.com>
2026-01-09 04:59:55 +0100

    adaptation du code et parametres

--- src/sort.c
+++ src/sort.c
@@ -29,102 +29,7 @@ void sortAlphabetically(Student_Management *management ){
      
 
     }
-
-    // Par ordre alphabetique;
-
-void sortAlphabetically(Student_Management *management , int nombre){
-
-
-
-   for (int i = 1; i < nombre; i++)
-   {
-     
-      Student_Management stud  = management[i];
-       
-       
-      int j = i-1;// intialistion du compteur des cases 
-suivantes;
-
-      while((j>=0)&&(strcmp(stud.matricule,management[j].
-matricule)< 0)){
-       management[j+1] = management[j];//Remplacement de la casesuivante par la precedente;
-
-       j--;
-      }
-     
-        management[j+1] = stud;// positionnement de la case dans le rang;
-     
-
-
-     
-     
-     
-     
-     
-      /* code */
    }
-
-
-}
-
-
-
-/**
- * Tri par filière
- * 
- * @param management Pointeur vers la structure de gestion
- */
-void sortByOption(Student_Management *management m, int nombre)
-{
-  
-   for (int i = 1; i < nombre; i++)
-   {
-     
-      Student_Management stud  = management[i];
-       
-       
-      int j = i-1;// intialistion du compteur des cases suivantes;
-
-      while((j>=0)&&(strcmp(stud.Option,management[j].
-Option)< 0)){
-       management[j+1] = management[j];//Remplacement de la casesuivante par la precedente;
-
-       j--;
-      }
-     
-        management[j+1] = stud;// positionnement de la case dans le rang;
-     
-
-
-     
-     
-     
-     
-     
-      /* code */
-   }
-};
-
-/**
- * Tri par matricule (pour recherche dichotomique)
- * @param management Pointeur vers la structure de gestion
- */
-void sortById(Student_Management *management , int nombre){
-  
-
-     
-     
-     
-     
-     
-      /* code */
-   }
-
-
-}
-
-
-
 /**
  * Tri par filière
  * 
@@ -174,23 +79,6 @@ int nombre = 0;
 
 nombre = management->number;
 
-
-
-   for (int i = 1; i < nombre; i++)
-   {
-     
-      Student_Management stud  = management[i];
-       
-       
-      int j = i-1;// intialistion du compteur des cases suivantes;
-
-      while((j>=0)&&(strcmp(stud.matricule,management[j].matricule)< 0)){
-       management[j+1] = management[j];//Remplacement de la casesuivante par la precedente;
-
-       j--;
-      }
-     
-        management[j+1] = stud;// positionnement de la case dans le rang;
      
 
    for (int i = 1; i < nombre; i++)
@@ -201,8 +89,6 @@ nombre = management->number;
        
       int j = i-1;// intialistion du compteur des cases suivantes;
 
-<<<<<<< HEAD
-=======
       while((j>=0)&&(strcmp(stud.id,management->list[j].id)< 0)){
        management->list[j+1] = management->list[j];//Remplacement de la casesuivante par la precedente;
 
@@ -213,7 +99,7 @@ nombre = management->number;
      
 
 
->>>>>>> main
+
      
      
      
@@ -224,8 +110,7 @@ nombre = management->number;
    
     
 
-<<<<<<< HEAD
+
 };
-=======
-};
->>>>>>> main
+
+
